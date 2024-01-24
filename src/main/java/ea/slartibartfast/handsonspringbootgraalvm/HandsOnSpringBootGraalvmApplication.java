e57f66f196ce71c6657cb25eb07c262305bb8382
Erdem Akdogan <erdem.akdogan@manigo.com>
2024-01-24 14:30:17 +0300

    inits app

---
+++ src/main/java/ea/slartibartfast/handsonspringbootgraalvm/HandsOnSpringBootGraalvmApplication.java
@@ -0,0 +1,13 @@
+package ea.slartibartfast.handsonspringbootgraalvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandsOnSpringBootGraalvmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandsOnSpringBootGraalvmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ea/slartibartfast/handsonspringbootgraalvm/HandsOnSpringBootGraalvmApplicationTests.java
@@ -0,0 +1,13 @@
+package ea.slartibartfast.handsonspringbootgraalvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandsOnSpringBootGraalvmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
